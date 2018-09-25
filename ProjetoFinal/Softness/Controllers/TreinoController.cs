32601cc6949d3c197889f0a4643b6086996029ae
v010 <v010@benner.com.br>
2018-09-25 17:30:30 -0300

    Fazendo View Treino

---
+++ ProjetoFinal/Softness/Controllers/TreinoController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Softness.Controllers
+{
+    public class TreinoController : Controller
+    {
+        // GET: Treino
+        public ActionResult NovoTreino()
+        {
+            return View();
+        }
+    }
+}
